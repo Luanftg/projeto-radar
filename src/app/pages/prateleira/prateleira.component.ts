7b98df4aecaf0d33bb6fdf9b5395585493658e5c
GuiHSM <Guihenrisampaio@gmail.com>
2023-01-21 14:27:06 -0300

    fix(#24): arrumando bug que não salva as posições corretamente

--- src/app/pages/prateleira/prateleira.component.ts
+++ src/app/pages/prateleira/prateleira.component.ts
@@ -26,21 +26,7 @@ export class PrateleiraComponent implements OnInit {
   estoque!: Medicamento[];
   ngOnInit(): void {
   }
-  prateleira11: Medicamento[] = [];
-  prateleira12: Medicamento[] = [];
-  prateleira13: Medicamento[] = [];
-  prateleira14: Medicamento[] = [];
-  prateleira15: Medicamento[] = [];
-  prateleira21: Medicamento[] = [];
-  prateleira22: Medicamento[] = [];
-  prateleira23: Medicamento[] = [];
-  prateleira24: Medicamento[] = [];
-  prateleira25: Medicamento[] = [];
-  prateleira31: Medicamento[] = [];
-  prateleira32: Medicamento[] = [];
-  prateleira33: Medicamento[] = [];
-  prateleira34: Medicamento[] = [];
-  prateleira35: Medicamento[] = [];
+  prateleiras: Medicamento[][]=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
   prateleira:any[] = [];
 
   // Inicia a instância de getMedicamentos
@@ -67,7 +53,6 @@ export class PrateleiraComponent implements OnInit {
       console.log(
         "ID do Container: " + containerId + ", Index do Container: " + indexContainer + ", ID do item: " + itemId
       )
-      if(containerId != 0) this.prateleira.push({containerId, indexContainer, itemId})
     } else  if((containerId!=0&&event.container.data.length<4)||containerId===0){
       transferArrayItem(
         event.previousContainer.data,
@@ -75,33 +60,21 @@ export class PrateleiraComponent implements OnInit {
         event.previousIndex,
         event.currentIndex,
       );
-      containerId = Number(event.container.id) //id do container onde o item foi dropado
-      indexContainer = Number(event.currentIndex) //Index da posição do item dentro do container
-      itemId = Number(event.container.data[event.currentIndex]['id']) //ID do item movid
-      console.log(
-        "ID do Container: " + containerId + ", Index do Container: " + indexContainer + ", ID do item: " + itemId
-      )
-      if(containerId != 0) {
-        let existe=false;
-        for (let i = 0; i < this.prateleira.length; i++) {
-          if(this.prateleira[i]["itemId"]===itemId){
-            existe =true;
-            this.prateleira[i]={containerId, indexContainer, itemId};
-          }
-          
-        }
-        if(!existe) this.prateleira.push({ containerId, indexContainer, itemId})
-      }else{
-        for (let i = 0; i < this.prateleira.length; i++) {
-          if(this.prateleira[i]["itemId"]===itemId){
-            this.prateleira.splice(i,1);
-          }
-        }
-      }
     }
   }
   
   salvar() {
+    this.prateleira=[];
+    for (let i = 0; i < 15; i++) {
+      const elementos = this.prateleiras[i];
+      for (let j = 0; j < elementos.length; j++) {
+        const elemento = elementos[i];
+        let containerId = i;
+        let indexContainer = j;
+        let itemId = elemento.id;
+        this.prateleira.push({containerId, indexContainer, itemId})
+      }
+    }
     console.log(this.prateleira)
   }
 }
